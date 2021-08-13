213111ec133feba3153db8d01b004c297d53abce
Hernando Abella <hernandoabella@gmail.com>
2021-08-13 13:28:57 -0500

    some practice with js

--- js/program1.js
+++ js/program1.js
@@ -2,7 +2,78 @@
 
 
 // 1. Programa Hola Mundo
-console.log("Hola Mundo");
+// console.log("Hola Mundo");
 
 // Hola Mundo
+
+// 2. Suma dos números
+
+// const numero1 = 5;
+// const numero2 = 3;
+
+// Suma los dos números
+
+// const suma = numero1 + numero2;
+
+// Muestra el resultado de la Suma
+
+// console.log('La suma de ' + numero1 + ' ' + numero2 + " es igual a: " + suma); 
+
+// Suma dos números ingresados por el usuario
+
+// const numero1 = parseInt(prompt('Ingresa el primer número'));
+// const numero2 = parseInt(prompt('Ingresa el segundo número'));
+
+// Suma los números
+
+// const suma = numero1 + numero2;
+
+// Muestra el resultado
+
+// console.log(`La suma de ${numero1} y ${numero2} es ${suma}`);
+
+// 3. Encuentra la raíz cuadrada
+
+// Obtén los datos ingresados por el usuario
+
+// const prompt("Ingrese el número");
+
+// const resultado = Math.sqrt(33);
+
+// console.log(`La raíz cuadrada de ${numero} es ${resultado}`);
+
+// const numero1 = 2.25;
+// const numero2 = -4;
+// const numero3 = 'hello';
+
+// const resultado1 = Math.sqrt(numero1);
+// const resultado2 = Math.sqrt(numero2);
+// const resultado3 = Math.sqrt(numero3);
+
+// console.log(`La raíz cuadrada de ${numero1} es ${resultado1}`);
+// console.log(`La raíz cuadrada de ${numero2} es ${resultado2}`);
+// console.log(`La raíz cuadrada de ${numero3} es ${resultado3}`);
+
+// const valorBase = prompt("Ingrese la baase del triángulo: ");
+// const valorAltura = prompt("Ingrese la altura del triángulo: ");
+
+// // Calcula el área
+
+// const valorArea = (valorBase * valorAltura) / 2;
+
+// console.log(`El área del triangulo es ${valorArea}`);
+
+// const lado1 = parseInt(prompt('Ingresa el lado 1: '));
+// const lado2 = parseInt(prompt('Ingresa el lado 2: '));
+// const lado3 = parseInt(prompt('Ingresa el lado 3: '));
+
+// Calcula el semi-perímetro
+
+// const s = (lado1 + lado2 + lado3) / 2;
+
+// Calcula el area 
+
+// const valorArea = Math.sqrt(s * (s - lado1) * (s - lado2) * (s - lado3));
+
+// console.log(`El área del triángulo es ${valorArea}`);
 
